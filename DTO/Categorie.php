ac65a8dc3e2cddb82a3c4ccd2b9e60690c80afe8
LorisTrr <loristerry63430@gmail.com>
2021-04-02 14:07:01 +0200

    dto

---
+++ DTO/Categorie.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class Categorie
+{
+    private $idCategorie;
+    private $nom;
+
+    /**
+     * Categorie constructor.
+     * @param $idCategorie
+     * @param $nom
+     */
+    public function __construct($idCategorie, $nom)
+    {
+        $this->idCategorie = $idCategorie;
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdCategorie()
+    {
+        return $this->idCategorie;
+    }
+
+    /**
+     * @param mixed $idCategorie
+     */
+    public function setIdCategorie($idCategorie)
+    {
+        $this->idCategorie = $idCategorie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+}
