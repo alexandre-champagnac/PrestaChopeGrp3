8e05ca7531f6cbadf89ab33b061a03ac445fac79
LorisTrr <loristerry63430@gmail.com>
2021-04-09 15:23:10 +0200

    fe

--- pages/connexion/ControleurConnexion.php
+++ pages/connexion/ControleurConnexion.php
@@ -11,24 +11,23 @@ class ControleurConnexion
         include_once ("DAO/ClientDAO.php");
         $verif = ClientDAO::connexion($pseudo, $mdp);
         var_dump($verif);
-
-        if ($verif)
-        {
-            $_SESSION['idClient'] = $verif->getIdClient();
-            $_SESSION['prenom'] = $verif->getPrenom();
-            $_SESSION['nom'] = $verif->getNom();
-            $_SESSION['pseudo'] = $verif->getPseudo();
-            $_SESSION['mail'] = $verif->getMail();
-            $_SESSION['avatar'] = $verif->getAvatar();
-            $_SESSION['cagnotte'] = $verif->getCagnotte();
-            $_SESSION['isAdmin'] = $verif->getIsAdmin();
-            $_SESSION['adresse'] = $verif->getAdresse();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+            if ($verif)
+            {
+                $_SESSION['idClient'] = $verif->getIdClient();
+                $_SESSION['prenom'] = $verif->getPrenom();
+                $_SESSION['nom'] = $verif->getNom();
+                $_SESSION['pseudo'] = $verif->getPseudo();
+                $_SESSION['mail'] = $verif->getMail();
+                $_SESSION['avatar'] = $verif->getAvatar();
+                $_SESSION['cagnotte'] = $verif->getCagnotte();
+                $_SESSION['isAdmin'] = $verif->getIsAdmin();
+                $_SESSION['adresse'] = $verif->getAdresse();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
     }
 
     public function redirectUser()
