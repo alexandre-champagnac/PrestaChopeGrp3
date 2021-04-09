2274682802635502f96bf5ba2c930ecf45dc604e
LorisTrr <loristerry63430@gmail.com>
2021-04-09 15:31:27 +0200

    vcd

--- index.php
+++ index.php
@@ -1,22 +1,56 @@
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8" />
-		<title>World Bounty Hunters Agency</title>
-	</head>
-	<body>
+<head>
+    <meta charset="utf-8" />
+    <title>World Bounty Hunters Agency</title>
+</head>
+<body>
+<header>
+    <div class="banniere">
+        <img class="" src="" alt=""/>
+        <?php
+        $page = "connexion";
+        include_once("tools/SuperControleur.php");
+        if (!empty($_GET['page'])) {
+            $page = $_GET['page'];
+        }
+        if((!isset($_SESSION['pseudo']) && $page!='connexion')){
+            ?>
+            <button href="" class="button">se connecter</button>
+            <button href="" class="button">s'inscrire</button>
+            <?php
+        }
+        elseif(isset($_SESSION['pseudo'])){ ?>
+            <button href="" class="button">se deconnecter</button>
+            <?php
+        }
+        ?>
+    </div>
 
-<?php
-
+    <?php
     $page = "connexion";
-	include_once("tools/SuperControleur.php");
-  	if(!empty($_GET['page']))
-  	{
-		$page = $_GET['page'];
-	}
-  	SuperControleur::callPage($page);
+    include_once("tools/SuperControleur.php");
+    if(!empty($_GET['page']))
+    {
+        $page = $_GET['page'];
+    }
+    SuperControleur::callPage($page);
+    ?>
+</header>
+<main>
+    <?php
+        $page = "connexion";
+        include_once("tools/SuperControleur.php");
+        if (!empty($_GET['page'])) {
+            $page = $_GET['page'];
+        }
 
-?>
 
-	</body>
+        SuperControleur::callPage($page);
+        ?>
+</main>
+<footer>
+
+</footer>
+</body>
 </html>
