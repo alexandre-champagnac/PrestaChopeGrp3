f6b1e2b21e65895771ab8f1af9ebee834b70ebb1
LorisTrr <loristerry63430@gmail.com>
2021-04-30 13:12:47 +0200

    fe

--- pages/connexion/connexion.php
+++ pages/connexion/connexion.php
@@ -4,20 +4,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width-device-width, initial-scale 1">
     <title>Connexion</title>
-    <link rel="stylesheet" href="../../assets/css/style_login.css">
+    <link rel="stylesheet" href="assets/css/style_login.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
 
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
 </head>
 <body>
-    <img class="wave" src="../../assets/img/wave.png" alt="">
+    <img class="wave" src="assets/img/wave.png" alt="">
     <div class="container">
         <div class="img">
-            <img src="../../assets/img/drink.svg" alt="">
+            <img src="assets/img/drink.svg" alt="">
         </div>
         <div class="loginform-container">
             <form action="index.php?page=connexion" method="post">
-                <img src="../../assets/img/logo_Prestachoppe.png" alt="" class="avatar">
+                <img src="assets/img/logo_Prestachoppe.png" alt="" class="avatar">
                 <h2>Bienvenue !</h2>
                 <div class="input-div one">
                     <div class="i">
@@ -42,6 +42,6 @@
             </form>
         </div>
     </div>
-    <script type="text/javascript" src="../../assets/js/login.js"></script>
+    <script type="text/javascript" src="assets/js/login.js"></script>
 </body>
 </html>
