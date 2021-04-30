bcef1000319320802ece54023166171213d7cc7d
Alexandre CHAMPAGNAC <alexch4mp@gmail.com>
2021-04-30 16:24:11 +0200

    Menu Repare

--- pages/accueil/accueil.php
+++ pages/accueil/accueil.php
@@ -56,11 +56,11 @@
     <section class="menu section bd-container" id="menu">
         <span class="section-subtitle">Special</span>
         <h2 class="section-title">Menu de la semaine</h2>
-
+        <div class="menu__container bd-grid">
         <?php $produits = ControllerAccueil::produit();
         foreach ($produits as $value){
             if($value->getQuantité() > 0){ ?>
-                <div class="menu__container bd-grid">
+
                     <div class="menu__content">
                         <img src="<?php echo $value->getPhoto(); ?>" alt="" class="menu__img">
                         <h3 class="menu__name"><?php echo $value->getNomProduit(); ?> </h3>
@@ -68,8 +68,9 @@
                         <span class="menu__preci"> <?php echo $value->getPrix(); ?>€</span>
                         <a href="index.php?page=voirProduit&id=<?php echo $value->getIdProduit() ; ?>" class="button menu__button"><i class='bx bx-cart-alt'></i></a>
                     </div>
-                </div>
+
         <?php }} ?>
+        </div>
     </section>
 
     <!--===== Section application =======-->
