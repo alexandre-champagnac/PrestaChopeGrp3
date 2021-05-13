946d6c32dcaa7e89b1127fa6087901ea5a06a1a1
LorisTrr <loristerry63430@gmail.com>
2021-05-13 19:11:58 +0200

    fe

--- DAO/CommandeDAO.php
+++ DAO/CommandeDAO.php
@@ -1,8 +1,17 @@
 <?php
-
-include_once 'DTO/CommandeDTO.php';
+include_once "tools/DatabaseLinker.php";
+include_once "DAO/ProduitDAO.php";
 
 class CommandeDAO
 {
-
+    public function commander($panier){
+        $nbproduit= count($panier);
+        $montantTotal = 0;
+        for($i = 0 ; $i < count($panier);$i++){
+            $produit = ProduitDAO::getProduitById($panier[0]);
+            $montantTotal = $produit->getPrix() + $panier[1];
+        }
+        $bdd = DatabaseLinker::getConnexion();
+        $
+    }
 }
--- scriptPrestachope.sql
+++ scriptPrestachope.sql
@@ -19,8 +19,9 @@ CREATE TABLE Clients(
                         password Varchar (255) NOT NULL ,
                         mail     Varchar (100) NOT NULL ,
                         adresse  Varchar (50) NOT NULL ,
-                        cagnotte Float NOT NULL DEFAULT 50,
-                        isAdmin  Int NOT NULL DEFAULT 0
+                        cagnotte Float NOT NULL ,
+                        avatar   Text NOT NULL ,
+                        isAdmin  Int NOT NULL
     ,CONSTRAINT Clients_PK PRIMARY KEY (idClient)
 )ENGINE=InnoDB;
 
@@ -31,28 +32,10 @@ CREATE TABLE Clients(
 
 CREATE TABLE Produit(
                         idProduit      Int  Auto_increment  NOT NULL ,
-                        nom            Varchar (50) NOT NULL ,
                         prix           Float NOT NULL ,
-                        quantite       Int NOT NULL ,
-                        photo          Text NOT NULL ,
-                        description    Text NOT NULL
+                        datePeremption Date NOT NULL ,
+                        quantite       Int NOT NULL
     ,CONSTRAINT Produit_PK PRIMARY KEY (idProduit)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: Commande
-#------------------------------------------------------------
-
-CREATE TABLE Commande(
-                         idCommande   Int  Auto_increment  NOT NULL ,
-                         nbProduit    Int NOT NULL ,
-                         montant      Float NOT NULL ,
-                         dateCommande TimeStamp NOT NULL ,
-                         idClient     Int NOT NULL
-    ,CONSTRAINT Commande_PK PRIMARY KEY (idCommande)
-
-    ,CONSTRAINT Commande_Clients_FK FOREIGN KEY (idClient) REFERENCES Clients(idClient)
 )ENGINE=InnoDB;
 
 
@@ -61,8 +44,7 @@ CREATE TABLE Commande(
 #------------------------------------------------------------
 
 CREATE TABLE categorie(
-                          idCategorie  Int  Auto_increment  NOT NULL ,
-                          nomCategorie Varchar (50) NOT NULL
+                          idCategorie Int  Auto_increment  NOT NULL
     ,CONSTRAINT categorie_PK PRIMARY KEY (idCategorie)
 )ENGINE=InnoDB;
 
@@ -77,9 +59,6 @@ CREATE TABLE commentaire(
                             idProduit     Int NOT NULL ,
                             idClient      Int NOT NULL
     ,CONSTRAINT commentaire_PK PRIMARY KEY (idCommentaire)
-
-    ,CONSTRAINT commentaire_Produit_FK FOREIGN KEY (idProduit) REFERENCES Produit(idProduit)
-    ,CONSTRAINT commentaire_Clients0_FK FOREIGN KEY (idClient) REFERENCES Clients(idClient)
 )ENGINE=InnoDB;
 
 
@@ -96,20 +75,6 @@ CREATE TABLE Entreprise(
 
 
 #------------------------------------------------------------
-# Table: contient
-#------------------------------------------------------------
-
-CREATE TABLE contient(
-                         idProduit  Int NOT NULL ,
-                         idCommande Int NOT NULL
-    ,CONSTRAINT contient_PK PRIMARY KEY (idProduit,idCommande)
-
-    ,CONSTRAINT contient_Produit_FK FOREIGN KEY (idProduit) REFERENCES Produit(idProduit)
-    ,CONSTRAINT contient_Commande0_FK FOREIGN KEY (idCommande) REFERENCES Commande(idCommande)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
 # Table: Appartient
 #------------------------------------------------------------
 
@@ -117,8 +82,98 @@ CREATE TABLE Appartient(
                            idProduit   Int NOT NULL ,
                            idCategorie Int NOT NULL
     ,CONSTRAINT Appartient_PK PRIMARY KEY (idProduit,idCategorie)
-
-    ,CONSTRAINT Appartient_Produit_FK FOREIGN KEY (idProduit) REFERENCES Produit(idProduit)
-    ,CONSTRAINT Appartient_categorie0_FK FOREIGN KEY (idCategorie) REFERENCES categorie(idCategorie)
 )ENGINE=InnoDB;
 
+
+#------------------------------------------------------------
+# Table: Commande
+#------------------------------------------------------------
+
+CREATE TABLE Commande(
+                         idCommande   Int  Auto_increment  NOT NULL ,
+                         nbProduit    Int NOT NULL ,
+                         montant      Float NOT NULL ,
+                         dateCommande TimeStamp NOT NULL ,
+                         idClient     Int NOT NULL ,
+                         idFacture    Int NOT NULL
+    ,CONSTRAINT Commande_PK PRIMARY KEY (idCommande)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: facture
+#------------------------------------------------------------
+
+CREATE TABLE facture(
+                        idFacture  Int  Auto_increment  NOT NULL ,
+                        montant    Float NOT NULL ,
+                        idCommande Int NOT NULL
+    ,CONSTRAINT facture_PK PRIMARY KEY (idFacture)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: ProduitCommande
+#------------------------------------------------------------
+
+CREATE TABLE ProduitCommande(
+                                idProduit  Int NOT NULL ,
+                                idCommande Int NOT NULL
+    ,CONSTRAINT ProduitCommande_PK PRIMARY KEY (idProduit,idCommande)
+)ENGINE=InnoDB;
+
+
+
+
+ALTER TABLE commentaire
+    ADD CONSTRAINT commentaire_Produit0_FK
+        FOREIGN KEY (idProduit)
+            REFERENCES Produit(idProduit);
+
+ALTER TABLE commentaire
+    ADD CONSTRAINT commentaire_Clients1_FK
+        FOREIGN KEY (idClient)
+            REFERENCES Clients(idClient);
+
+ALTER TABLE Appartient
+    ADD CONSTRAINT Appartient_Produit0_FK
+        FOREIGN KEY (idProduit)
+            REFERENCES Produit(idProduit);
+
+ALTER TABLE Appartient
+    ADD CONSTRAINT Appartient_categorie1_FK
+        FOREIGN KEY (idCategorie)
+            REFERENCES categorie(idCategorie);
+
+ALTER TABLE Commande
+    ADD CONSTRAINT Commande_Clients0_FK
+        FOREIGN KEY (idClient)
+            REFERENCES Clients(idClient);
+
+ALTER TABLE Commande
+    ADD CONSTRAINT Commande_facture1_FK
+        FOREIGN KEY (idFacture)
+            REFERENCES facture(idFacture);
+
+ALTER TABLE Commande
+    ADD CONSTRAINT Commande_facture0_AK
+        UNIQUE (idFacture);
+
+ALTER TABLE facture
+    ADD CONSTRAINT facture_Commande0_FK
+        FOREIGN KEY (idCommande)
+            REFERENCES Commande(idCommande);
+
+ALTER TABLE facture
+    ADD CONSTRAINT facture_Commande0_AK
+        UNIQUE (idCommande);
+
+ALTER TABLE ProduitCommande
+    ADD CONSTRAINT ProduitCommande_Produit0_FK
+        FOREIGN KEY (idProduit)
+            REFERENCES Produit(idProduit);
+
+ALTER TABLE ProduitCommande
+    ADD CONSTRAINT ProduitCommande_Commande1_FK
+        FOREIGN KEY (idCommande)
+            REFERENCES Commande(idCommande);
