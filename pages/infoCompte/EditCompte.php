9619c9f48a71b9404d2759f701f0b7b74c3a39de
Alexandre CHAMPAGNAC <alexch4mp@gmail.com>
2021-05-14 17:18:52 +0200

    Light/Dark Theme Repare

--- pages/infoCompte/EditCompte.php
+++ pages/infoCompte/EditCompte.php
@@ -1,3 +1,9 @@
+<?php
+include_once 'DAO/ClientDAO.php';
+include_once 'DTO/ClientDTO.php';
+$client = ClientDAO::getInfos($_SESSION['idClient']);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,16 +12,32 @@
 </head>
 <body>
     <center>
-        <div class="box">
-            <img src="assets/img/profil.svg" alt="">
-            <input type="text" name="" placeholder="Pseudo">
-            <input type="email" name="" placeholder="Mail">
-            <input type="text" name="" placeholder="Adresse">
-            <input type="text" name="" placeholder="Prenom">
-            <input type="text" name="" placeholder="Nom">
-            <button style="float: left;margin:10px 0 0 18.2%;">Annuler</button>
-            <button style="float: left;margin:10px 18.2% 0 0;">Modifier</button>
-        </div>
+        <form action="index.php?page=modifcompte" method="post">
+            <div class="box">
+
+                <label for="nom">Modifier votre Nom :</label>
+                <input type="text" id="nom" name="nom" minlength="3" value="<?php echo $client->getNom(); ?>"/>
+
+                <label for="Prenom">Modifier votre Prenom :</label>
+                <input type="text" id="prenom" name="prenom" value="<?php echo $client->getPrenom(); ?>"/>
+
+                <label for="adresse">Modifier votre Adresse :</label>
+                <input type="text" id="adresse" name="adresse" value="<?php echo $client->getAdresse(); ?>"/>
+
+                <label for="mail">Modifier votre Mail :</label>
+                <input type="text" id="mail" name="mail" value="<?php echo $client->getMail(); ?>"/>
+
+                <label for="pseudo">Modifier votre Pseudo :</label>
+                <input type="text" id="pseudo" name="pseudo" value="<?php echo $client->getPseudo(); ?>"/>
+
+                <label for="password">Nouveau MDP</label>
+                <input type="password" id="password" name="password"/>
+
+                <label for="password">Confirmer MDP</label>
+                <input type="password" id="newpassword" name="newpassword"/>
+
+                <input type="submit"  class="submitbutton" value="OK">
+            </div>
     </center>
 </body>
 </html>
